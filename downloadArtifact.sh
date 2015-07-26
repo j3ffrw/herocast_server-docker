0f65d59dfd5b374ad2a7d7c6c45a7f14b61ff597
jefffermo <jefffermo@gmail.com>
2015-07-26 10:29:41 +0800

    test env variables

--- downloadArtifact.sh
+++ downloadArtifact.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-shpApiToken=$1
-BUILD_ID=$2
+echo "api: $shpApiToken"
 
 curl -o Build.tar.gz `curl -H "Authorization: apiToken $shpApiToken" -X GET https://api.shippable.com/builds/$BUILD_ID/1/artifacts`
