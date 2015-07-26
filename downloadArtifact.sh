9498b6a07134033c0c22c7f9e3688a7a386625bf
jefffermo <jefffermo@gmail.com>
2015-07-26 10:47:47 +0800

    fix path of packed node app

--- downloadArtifact.sh
+++ downloadArtifact.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-echo "api: $shpApiToken"
-echo "build: $BUILD_ID"
-
 curl -o Build.tar.gz `curl -H "Authorization: apiToken $shpApiToken" -X GET https://api.shippable.com/builds/$BUILD_ID/1/artifacts`
